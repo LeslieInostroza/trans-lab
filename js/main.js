a3ce922e82f373762c638593bb81542f117c5c76
Leslie <leslie.ino@gmail.com>
2018-06-27 15:33:41 -0400

    validacion email

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 /* Holiii acá va tu código también */
+
+let emailContacto = document.getElementById('emailContacto');
+let password = document.getElementById('password');
+const btnPassword = document.getElementById('btnPassword');
+let filter = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+//Validar email
+btnPassword.addEventListener('click', () =>{
+  if(emailContacto.value === '' || password.value === ''){
+    }
+    if(!filter.test(emailContacto.value)){
+      alert('Correo No Valido');
+    }
+});
